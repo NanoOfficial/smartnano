c088343dc2c9ad4be18b03178477d6434bc223da
krishpranav <krisna.pranav@gmail.com>
2023-05-17 17:47:06 +0530

    ibc: utils

--- lib/core/ibc/utils.go
+++ lib/core/ibc/utils.go
@@ -7,7 +7,11 @@
 
 package ibc
 
-import "github.com/NanoOfficial/smartnano/lib/core/packets"
+import (
+	"github.com/NanoOfficial/smartnano/lib/core/packets"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
+)
 
 type capturePacketSender struct {
 	inner   packets.PacketSender
@@ -16,8 +20,20 @@ type capturePacketSender struct {
 
 var _ packets.PacketSender = (*capturePacketSender)(nil)
 
-func newCapturePacketSender(ps packets.PacketSender) *capturePacketSender {
+func NewCapturePacketSender(ps packets.PacketSender) *capturePacketSender {
 	return &capturePacketSender{inner: ps}
+}
+
+func (ps *capturePacketSender) SendPacket(
+	ctx sdk.Context,
+	channelCap *capabilitytypes.Capability,
+	packet packets.OutgoingPacket,
+) error {
+	if err := ps.inner.SendPacket(ctx, channelCap, packet); err != nil {
+		return err
+	}
+	ps.packets = append(ps.packets, packet)
+	return nil
 }
 
 func (ps *capturePacketSender) Packets() []packets.OutgoingPacket {
